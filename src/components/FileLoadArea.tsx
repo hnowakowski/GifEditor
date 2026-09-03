718a2fd7da991719eef13df2b4fda2a96963c185
hnowakowski <h.nowakowski.praca@gmail.com>
2026-09-03 17:48:15 +0200

    Fix deserialization transparency issue

--- src/components/FileLoadArea.tsx
+++ src/components/FileLoadArea.tsx
@@ -5,74 +5,63 @@ const fr = new FileReader();
 function readFile(imgCallback: Function){
     const imgPreview = document.getElementById("imgPreview") as HTMLImageElement;
     const fileUpload = document.getElementById("fileUpload") as HTMLInputElement;
-    const tempCanvas = document.getElementById("tempCanvas") as HTMLCanvasElement;
-    const tempCtx = tempCanvas.getContext("2d");
-    if (imgPreview && fileUpload && fileUpload.files && tempCanvas && tempCtx){
+
+    // hidden "master" canvas that is updated on each frame
+    // its current state is later copied onto every canvas representing their respective frame
+    const currentStateCanvas = document.getElementById("currentStateCanvas") as HTMLCanvasElement;
+    const currentStateCtx = currentStateCanvas.getContext("2d")!;
+
+    // each frame records data for the delta after the previous frame
+    // represents an updated patch of the image to be later overlaid over the current state
+    const deltaCanvas = document.getElementById("deltaCanvas") as HTMLCanvasElement;
+    const deltaCtx = deltaCanvas.getContext("2d")!;
+
+    if (imgPreview && fileUpload && fileUpload.files && currentStateCanvas){
         fr.readAsArrayBuffer(fileUpload.files[0]);
-        fr.addEventListener("load", ()=>{
+        fr.addEventListener("load", () => {
             const buffer = fr.result as ArrayBuffer;
             if (buffer){
                 const gif = parseGIF(buffer);
+                currentStateCanvas.height = gif.lsd.height;
+                currentStateCanvas.width = gif.lsd.width;
                 const frames = decompressFrames(gif, true);
                 // frame.patch is essentially the raw uint8array with difference color info
-                console.log(frames.length);
+                
                 let imageData;
                 for (let i = 0; i < frames.length; i++){
-                    const dims = frames[i].dims; //very rarely the library misreads dimension data
-                    if (dims.width === 1 && dims.height === 1 && dims.top !== 0 && dims.left !== 0){
-                        dims.width += dims.left;
-                        dims.height += dims.top;
-                        dims.top = 0;
-                        dims.left = 0;
+                    const dims = frames[i].dims;
+                    
+                    if (!imageData || deltaCanvas.width != dims.width || deltaCanvas.height != dims.height){
+                        deltaCanvas.width = dims.width;
+                        deltaCanvas.height = dims.height;
+                        imageData = deltaCtx.createImageData(dims.width, dims.height);
                     }
+                    
+                    imageData.data.set(frames[i].patch);
+                    deltaCtx.putImageData(imageData, 0, 0);
 
+                    // disposal type 1 (most common) means delta, type 2 means full wipe
+                    if (i > 0){
+                        if (frames[i-1].disposalType ===  2) {
+                            const { width, height, left, top } = frames[i-1].dims;
+                            currentStateCtx.clearRect(left, top, width, height);
+                            console.log("Canvas wiped at " + i);
+                        }
+                    }
+                    
+                    currentStateCtx.drawImage(deltaCanvas, dims.left, dims.top);
+                    
+                    // create new canvas and copy frame image from current state
                     const canvas = document.createElement("canvas");
                     canvas.setAttribute("id", "c"+i);
-                    const ctx = canvas.getContext("2d");
-                    
-                    // preparing image entity(?) with a temp canvas
-                    canvas.width = dims.width;
-                    canvas.height = dims.height;
-                    
-                    if (!imageData || tempCanvas.width != dims.width || tempCanvas.height != dims.height){
-                        // if (!imageData)
-                        //     console.log("poop img data" + i);
-                        // if (tempCanvas.width != dims.width)
-                        //     console.log("poop width" + i);
-                        // if (tempCanvas.height != dims.height)
-                        //     console.log("poop height" + i);
-                        tempCanvas.width = dims.width;
-                        tempCanvas.height = dims.height;
-                        imageData = tempCtx.createImageData(dims.width, dims.height);
-                    }
-                    // if(frames[i].disposalType !== 1){
-                    //     console.log("Disposal type " + frames[i].disposalType + " at " + i);
-                    // }
-                    console.log(frames[i].transparentIndex + " " + i);
-                    console.log(frames[i].patch);
-
-                    imageData.data.set(frames[i].patch);
-                    tempCtx.putImageData(imageData, 0, 0);
-
-                    ctx?.drawImage(tempCanvas, dims.left, dims.top);
+                    canvas.width = gif.lsd.width;
+                    canvas.height = gif.lsd.height;
+                    const ctx = canvas.getContext("2d")!;
+                    ctx.drawImage(currentStateCanvas, 0, 0);
                     imgPreview.appendChild(canvas);
                 }
-                //
             }
         })
-
-
-        // SINGLE IMAGE TEST
-        // fr.readAsDataURL(fileUpload.files[0]);
-        // fr.addEventListener("load", () => {
-        //     const url = fr.result as string;
-        //     if (url){
-        //         imgPreview.src = url;
-        //         imgCallback(url);
-        //         console.log("updated image!");
-        //         //console.log(url);
-        //     }
-        // })
     }
     else{
         alert("no file!");
--- src/components/ImageManipulatorParent.tsx
+++ src/components/ImageManipulatorParent.tsx
@@ -4,13 +4,13 @@ import ScalableCanvas from './ScalableCanvas';
 
 export default function ImageManipulatorParent(){
     const [img, setImg] = useState("");
-    // TODO: make the temp canvas hidden later
     return (
         <>
         <FileLoadArea imgCallback={setImg}/>
         <div id="imgPreview"></div>
         <ScalableCanvas/>
-        <canvas id="tempCanvas"></canvas> 
+        <canvas id="currentStateCanvas" hidden></canvas> 
+        <canvas id="deltaCanvas" hidden></canvas>
         </>
     )
 }
